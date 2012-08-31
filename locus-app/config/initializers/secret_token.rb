1e0cf0e75fd657c5e4a16a08b652059a5bdbf508
Nick Recobra <oruen@undev.ru>
2012-08-31 14:59:58 +0400

    Тут будет аппа.

---
+++ locus-app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Locus::Application.config.secret_token = '82d3ac5d0312aa1b1661ec384097436cbf28bbe4f5d89bf12fd3d768d6aa79b666cee41de73a0a359669c4a9a130a2a02ef6eaecce7a1f828f767799fd8b1301'
