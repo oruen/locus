a4f2a6492c05ec2e94dd04886c735f126c9ca26a
Nick Recobra <oruen@undev.ru>
2012-08-31 19:58:24 +0400

    Список людей из контроллера.

--- locus-app/app/assets/javascripts/controllers/people_controller.js
+++ locus-app/app/assets/javascripts/controllers/people_controller.js
@@ -1,3 +1,9 @@
 Locus.set("PeopleController", Ember.ArrayController.extend({
+  content: [
+    {id: 1, name: "Nick Recobra"},
+    {id: 2, name: "Stepashka"},
+    {id: 3, name: "Сергей Париев"},
+    {id: 4, name: "Тимур Амиров"}
+  ]
 }));
 
